353b8bbc173a71d7b474b29ddd0864f87a6ecad4
yoshphys <84660663+yoshphys@users.noreply.github.com>
2024-01-27 13:31:07 +0900

    MaxTranslations increased
    The number of abstracts was increased from 10 to 20.

--- bin/arxiv_today.py
+++ bin/arxiv_today.py
@@ -11,28 +11,29 @@ from datetime import datetime, timedelta
 
 Categories = ["nucl-ex", "hep-ex", "nucl-th", "hep-th"]
 MaxResults = 10
-MaxTranslations = 10
+MaxTranslations = 20
 
-#__________________________________________________
+# __________________________________________________
+
 
 class MyHTMLParser(HTMLParser):
-
     tags = ["id", "published", "title", "name", "summary"]
     tag = ""
     eid = ""
-    flag    = dict()
-    entry   = dict()
+    flag = dict()
+    entry = dict()
     entries = dict()
-    dodump  = False
+    dodump = False
 
     def initialize(self):
         self.eid = ""
         self.tag = ""
         self.entry = dict()
-        self.flag  = dict()
+        self.flag = dict()
 
     def handle_starttag(self, tag, attrs):
-        if self.dodump: print("Encountered a start tag:", tag)
+        if self.dodump:
+            print("Encountered a start tag:", tag)
         self.flag[tag] = True
         if "entry" in self.flag.keys() and self.flag["entry"] and tag in self.tags:
             self.tag = tag
@@ -40,7 +41,8 @@ class MyHTMLParser(HTMLParser):
             self.tag = ""
 
     def handle_endtag(self, tag):
-        if self.dodump: print("Encountered an end tag :", tag)
+        if self.dodump:
+            print("Encountered an end tag :", tag)
         if "entry" in self.flag.keys() and self.flag["entry"] and "entry" == tag:
             self.entries[self.eid] = self.entry.copy()
             self.initialize()
@@ -49,11 +51,13 @@ class MyHTMLParser(HTMLParser):
         self.flag[tag] = False
 
     def handle_data(self, data):
-        if self.dodump: print("Encountered some data  :", data)
-        if not ("entry" in self.flag.keys() and self.flag["entry"]): return
+        if self.dodump:
+            print("Encountered some data  :", data)
+        if not ("entry" in self.flag.keys() and self.flag["entry"]):
+            return
 
         if "id" == self.tag and self.flag[self.tag]:
-            self.eid = data.split('/')[-1]
+            self.eid = data.split("/")[-1]
             self.entry["id"] = data
         elif "name" == self.tag and self.flag[self.tag]:
             # if "name" in self.entry.keys():
@@ -66,7 +70,9 @@ class MyHTMLParser(HTMLParser):
             else:
                 self.entry["name"] = [html.unescape(data)]
         elif self.tag in self.tags and self.flag[self.tag]:
-            self.entry[self.tag] = html.unescape(re.sub(r" {2,}", ' ', data.replace('\n', ' ').strip()))
+            self.entry[self.tag] = html.unescape(
+                re.sub(r" {2,}", " ", data.replace("\n", " ").strip())
+            )
         else:
             pass
 
@@ -76,18 +82,22 @@ class MyHTMLParser(HTMLParser):
     def do_dump(self, flag):
         self.dodump = flag
 
-#__________________________________________________
+
+# __________________________________________________
+
 
 def get_api_info(path):
     url = ""
     param = dict()
     with open(path, "r") as f:
         buff = json.load(f)
-        url = buff["scheme"] + "://" + '/'.join([buff["FQDN"], buff["path"]])
+        url = buff["scheme"] + "://" + "/".join([buff["FQDN"], buff["path"]])
         param["auth_key"] = buff["auth_key"]
     return url, param
 
-#__________________________________________________
+
+# __________________________________________________
+
 
 def translate(url, params, text):
     params["text"] = text
@@ -95,16 +105,17 @@ def translate(url, params, text):
     result = request.json()
     return result["translations"][0]["text"]
 
-#__________________________________________________
 
-ArxivApiUrl  = "http://export.arxiv.org/api"
+# __________________________________________________
+
+ArxivApiUrl = "http://export.arxiv.org/api"
 ArxivApiOptions = f"start=0&max_results={MaxResults}&sortBy=submittedDate"
 ArxivApiQueries = [f"cat:{icat}&{ArxivApiOptions}" for icat in Categories]
 
 DeeplApiConfigPath = os.path.join(os.path.dirname(__file__), "..", "api", "deepl.json")
 
-def main():
 
+def main():
     deeplurl, params = get_api_info(DeeplApiConfigPath)
     params["target_lang"] = "JA"
 
@@ -112,35 +123,39 @@ def main():
     for iquery in ArxivApiQueries:
         parser = MyHTMLParser()
         parser.do_dump(False)
-        iurl = '/'.join([ArxivApiUrl, f"query?search_query={iquery}"])
+        iurl = "/".join([ArxivApiUrl, f"query?search_query={iquery}"])
         buff = ""
         with libreq.urlopen(iurl) as response:
             buff = response.read().decode("utf-8")
         parser.feed(buff)
         entries.update(parser.get_list())
 
-
     theday = (datetime.utcnow().date() - timedelta(days=1)).isoformat()
 
     body = f"Papers submitted on {theday} UTC.\n\n"
 
-    n  = 0
+    n = 0
     nc0 = 0
     nc = 0
     for key, entry in entries.items():
-        updated = entry["published"].split('T')[0]
-        if updated != theday: continue
+        updated = entry["published"].split("T")[0]
+        if updated != theday:
+            continue
         inc = len(entry["summary"])
         nc0 += inc
-        body += 3 * '-' + '\n'
-        body += "## " + entry["title"] + '\n'
+        body += 3 * "-" + "\n"
+        body += "## " + entry["title"] + "\n"
         # body += "- published: " + entry["published"] + '\n'
-        body += "- author: " + ", ".join(entry["name"]) + '\n'
-        body += "- abstract: " + entry["summary"] + '\n'
+        body += "- author: " + ", ".join(entry["name"]) + "\n"
+        body += "- abstract: " + entry["summary"] + "\n"
         if n < MaxTranslations:
             nc += inc
-            body += "- transliation: " + translate(deeplurl, params, entry["summary"]) + '\n'
-        body += "- link: " + entry["id"] + '\n'
+            body += (
+                "- transliation: "
+                + translate(deeplurl, params, entry["summary"])
+                + "\n"
+            )
+        body += "- link: " + entry["id"] + "\n"
         body += "\n\n"
 
         n += 1
@@ -153,6 +168,7 @@ def main():
     else:
         print(body)
 
-#__________________________________________________
+
+# __________________________________________________
 
 main()
